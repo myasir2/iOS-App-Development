baf7c43d46f972bbfa939889749acec7fc98afdd
Yasir Merchant <yasir@Yasirs-MacBook-Pro.local>
2018-08-22 17:25:06 -0400

    Initial commit for twitter sentiment analysis app. App includes trained ML Model

---
+++ TwitterSentimentAnalysis/TwitterSentimentAnalysis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TwitterSentimentAnalysis
+//
+//  Created by Yasir Merchant on 2018-08-20.
+//  Copyright © 2018 Yasir Merchant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var inputTextLabel: UILabel!
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var emojiLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func predictButtonPressed(_ sender: Any)
+    {
+        
+    }
+}
+
