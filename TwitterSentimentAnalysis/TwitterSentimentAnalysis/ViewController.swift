03277a180c69c9a2b1edf0c903e65be521728a84
Yasir Merchant <yasir@yasirs-macbook-pro.local>
2018-09-13 21:31:09 -0400

    Added in swifter

--- TwitterSentimentAnalysis/TwitterSentimentAnalysis/ViewController.swift
+++ TwitterSentimentAnalysis/TwitterSentimentAnalysis/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController
 {
+    let swifter = Swifter(consumerKey: "DX3fo5lB8lLm56ahwRzmiCOED", consumerSecret: "S9YtQsDSjhddkgFshuGuJjXGye9xEF58j3HGvI7rFcg87OXHiQ")
+    
     @IBOutlet weak var inputTextLabel: UILabel!
     @IBOutlet weak var inputText: UITextField!
     @IBOutlet weak var emojiLabel: UILabel!
